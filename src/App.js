099f7a7e6baed43f4390e2ffecdb6427c37759bf
FlorianBorie <florian.borie@ynov.com>
2022-02-05 14:40:58 +0100

    add page dashboard

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 
 // Files
-import { PostList } from './components/posts';
+import { PostList, PostEdit, PostCreate  } from './components/posts';
 import { UserList } from './components/users';
+import Dashboard from './components/Dashboard';
+
+// Icons
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} />
-        <Resource name="users" list={UserList} />
-    </Admin>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
 );
 
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+export default () => (
+    <Card>
+        <CardHeader title="Bienvenue dans le panel D'administration de vos clients" />
+        <CardContent>Ici vous pouvez gérer les données de vos clients !</CardContent>
+    </Card>
+);
