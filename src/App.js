b65cc6237887367050bd958c235486d6d269291e
FlorianBorie <florian.borie@ynov.com>
2022-02-05 15:38:49 +0100

    add page connection

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate  } from './components/posts';
 import { UserList } from './components/users';
 import Dashboard from './components/Dashboard';
+import authProvider from './components/Auth/authProvider';
 
 // Icons
 import PostIcon from '@material-ui/icons/Book';
@@ -14,7 +15,7 @@ import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
